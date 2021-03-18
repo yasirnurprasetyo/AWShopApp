0cc34346554437f12d65cbdfa6d1d75c77c93088
yasirnurprasetyo <yasirnurprasetyo@gmail.com>
2021-03-18 15:40:20 +0700

    add buttom navigation activity

--- app/src/main/java/com/yasir/awshop/MainActivity.kt
+++ app/src/main/java/com/yasir/awshop/MainActivity.kt
@@ -2,10 +2,27 @@ package com.yasir.awshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.yasir.awshop.ui.fragments.AkunFragment
+import com.yasir.awshop.ui.fragments.HomeFragment
+import com.yasir.awshop.ui.fragments.KeranjangFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val fragmentHome : Fragment = HomeFragment()
+    val fragmentKeranjang : Fragment = KeranjangFragment()
+    val fragmentAkun : Fragment = AkunFragment()
+    val fm : FragmentManager = supportFragmentManager
+    var active : Fragment = fragmentHome
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
+        fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+
     }
 }
---
+++ app/src/main/java/com/yasir/awshop/ui/fragments/KeranjangFragment.kt
@@ -0,0 +1,18 @@
+package com.yasir.awshop.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.yasir.awshop.R
+
+class KeranjangFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_keranjang, container, false)
+    }
+}
